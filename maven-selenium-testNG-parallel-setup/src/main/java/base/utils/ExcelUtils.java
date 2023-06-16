c8b716246309b0d0a96745888b2c9c69858a981b
Trung Hieu <89647756+trunghieu-automate@users.noreply.github.com>
2023-06-16 09:17:40 +0700

    update ExcelUtils

--- maven-selenium-testNG-parallel-setup/src/main/java/base/utils/ExcelUtils.java
+++ maven-selenium-testNG-parallel-setup/src/main/java/base/utils/ExcelUtils.java
@@ -4,103 +4,120 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class ExcelUtils {
-    private static Workbook wb;
-    private static Sheet sh;
 
-    public static Object[][] getExcelData(String fileName, String sheetName, String testcases) {
-        Object[][] data = null;
-        Object[][] refinedData = null;
-        try {
-            FileInputStream fis = new FileInputStream(fileName);
-            wb = new XSSFWorkbook(fis);
-            sh = wb.getSheet(sheetName);
-            Row row = sh.getRow(0);
-
-            int noOfRows = sh.getPhysicalNumberOfRows();
-            int noOfCols = row.getLastCellNum();
-
-            Cell cell;
-            data = new Object[noOfRows - 1][noOfCols];
-            for (int i = 1; i < noOfRows; i++) {
-                for (int j = 0; j < noOfCols; j++) {
-                    row = sh.getRow(i);
-                    cell = row.getCell(j);
-                    data[i - 1][j] = cellToString(cell);
-                }
-            }
-            refinedData = new Object[][]{
-                    Arrays.stream(data)
-                            .filter(r -> String.valueOf(r[0]).equalsIgnoreCase(testcases)) // filter row that first element of row that equal testcases name
-                            .flatMap(Arrays::stream)
-                            .dropWhile(e -> String.valueOf(e).equalsIgnoreCase(testcases)) // drop the first element also
-                            .toArray()
-            };
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return refinedData;
-    }
-
-    public static Object[][] getDataForSpecificTest(String filename, String sheetname, String testname) {
+    /**
+     * @Description: return Object[1][] for @DataProvider, the second dimension is all the row data of testcase's name
+     * @Param: filename: path to excels file to read and update
+     * tcToFetch: take a method name to write its results
+     * result: result value to write
+     * @Author: hieu.trungvu
+     **/
+    public static Object[][] getDataForSpecificTest(String filename, String sheetName, String testName) {
         Workbook wb;
         Sheet sh;
-
         Object[][] data = null;
         try {
             FileInputStream file = new FileInputStream(filename); // open the Excel file
             wb = new XSSFWorkbook(file); // create a workbook object
-            sh = wb.getSheet(sheetname);
-            String text = "Doe"; // the text to match
+            sh = wb.getSheet(sheetName);
             ArrayList<Object> result = new ArrayList<>(); // a list to store the result
             for (Row row : sh) { // iterate over each row
                 Cell cell = row.getCell(0); // get the first cell
-                if (cellToString(cell).equalsIgnoreCase(testname)) { // check if the cell value matches the text
+                if (cellToString(cell).equalsIgnoreCase(testName)) { // check if the cell value matches the text case
                     for (int i = 1; i < row.getLastCellNum(); i++) { // iterate over the remaining cells in the row
                         result.add(cellToString(row.getCell(i))); // add the cell value to the result list
                     }
                     break;
                 }
             }
-            data = new Object[][]{result.toArray()};
+            data = new Object[][]{result.toArray()}; // convert array to 2 dimension object for data provider
         } catch (Exception e) {
             e.printStackTrace();
         }
         return data;
     }
 
-    public static Object[][] getExcelData(String fileName, String sheetName) {
-        Object[][] data = null;
+
+    /**
+     * @Description: update test result to excel data file at the last column for specific testcases name.
+     * @Param: filename: path to excels file to read and update
+     * tcToFetch: take a method name to write its results
+     * result: result value to write
+     * @Author: hieu.trungvu
+     **/
+    public static void updateTestResultToExcelFile(String filename, String tcToFetch, String result) {
+        Workbook wb;
         try {
-            FileInputStream fis = new FileInputStream(fileName);
-            wb = new XSSFWorkbook(fis);
-            sh = wb.getSheet(sheetName);
-            Row row = sh.getRow(0);
-
-            int noOfRows = sh.getPhysicalNumberOfRows();
-            int noOfCols = row.getLastCellNum();
-
-            Cell cell;
-            data = new Object[noOfRows - 1][noOfCols];
-            for (int i = 1; i < noOfRows; i++) {
-                for (int j = 0; j < noOfCols; j++) {
-                    row = sh.getRow(i);
-                    cell = row.getCell(j);
-                    data[i - 1][j] = cellToString(cell);
+            FileInputStream file = new FileInputStream(filename); // open the Excel file
+            wb = WorkbookFactory.create(file);
+            // loop through every sheet to find testcase's name
+            boolean resultCellExist = false;
+            for (Sheet sheet : wb) {
+                int colIndex = 0;
+                int rowIndex = 0;
+                if (sheet == null) {
+                    System.out.println("Unable to find the File/Sheet. Pls check if Sheet exists");
+                } else {
+                    boolean headerExist = false;
+                    // loop through first row to get colIndex
+                    for (Cell cell : sheet.getRow(0)) {
+                        // colIndex will be same with Test Result column or last column for the first time
+                        if (cellToString(cell).equalsIgnoreCase("Test Result")) {
+                            colIndex = cell.getColumnIndex();
+                            headerExist = true; // if not found, set true value
+                        } else {
+                            colIndex = sheet.getRow(0).getLastCellNum();
+                        }
+                    }
+                    // loop through every cell that equal specific value then get row index
+                    for (Row r : sheet) {
+                        for (Cell c : r) {
+                            if (cellToString(c).equalsIgnoreCase(tcToFetch)) {
+                                rowIndex = c.getRowIndex();// if it's the first time, create first row header, if not set result to proper cell
+                                resultCellExist = true; // turn true if found
+                                break; //break the for loop when found testcases name
+                            }
+                        }
+                    }
+                    // Create header if not founded header, and update result only if found proper testcase's name
+                    if (resultCellExist) {
+                        if (!headerExist) {
+                            sheet.getRow(0).createCell(colIndex).setCellValue("Test Result");
+                            System.out.println("Added Test result column @sheet: " + sheet.getSheetName() + " and @col:" + colIndex);
+                        }
+                        sheet.getRow(rowIndex).createCell(colIndex).setCellValue(result);
+                        System.out.println("Update Test result for " + tcToFetch + ", at cell (" + rowIndex + ", " + colIndex + ")");
+                        break; // break after updated
+                    }
+                }
+            }
+            // write file only if the proper testcase's name is found
+            if (resultCellExist) {
+                try {
+                    file.close();
+                    FileOutputStream out = new FileOutputStream(filename);
+                    wb.write(out);
+                    out.close();
+                    wb.close();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        return data;
     }
 
-
-    public static String cellToString(Cell cell) {
+    /**
+     * @Description: method return string of all type of value
+     * @Param: cell
+     * @Author: hieu.trungvu
+     **/
+    private static String cellToString(Cell cell) {
         CellType type = cell.getCellType();
         Object result = switch (type) {
             case NUMERIC -> // numeric value in excel
@@ -120,5 +137,4 @@ public class ExcelUtils {
         };
         return result.toString();
     }
-
 }
